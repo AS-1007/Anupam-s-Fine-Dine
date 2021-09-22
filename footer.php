72fcbf84077f0798d40ed039fa8036c6bd3833ab
Anupam Srivastava <srivastavanupam9473@gmail.com>
2021-09-22 16:33:10 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,52 @@
+<link rel="stylesheet" type="text/css" href="../style.css">
+
+<footer>
+	<div class="container-fluid">
+    <div class="row">
+
+    	<div class="col-md-4">
+    <h3 class="text-center"><strong><span class="glyphicon glyphicon-map-marker"></span></strong><br>
+    	BMSIT College Campus FoodCourt Avalahalli Bangalore 560064
+    </h3>
+    	</div>
+    	<div class="col-md-4">
+       <h3 class="text-center"><strong><span class="glyphicon glyphicon-earphone"></span></strong><br>
+       +91 9473905798 </h3>
+
+    	</div>
+    	
+    	<div class="col-md-4">
+    		<h3 class="text-center"><strong><span class="glyphicon glyphicon-glass"></span></strong><br> 24 Hours</h3>
+    		<div class="timing text-center">
+           
+    	</div>
+
+    	</div>
+    	
+
+    </div>
+    <div class="copuright-info container-fluid">
+      <?php
+      include 'assests/includes/copyright-info.php';
+      ?>
+    </div>
+    <div class="row footer-buttom">
+    	<div class="container">
+    	<div class="col-md-6">
+    	<h4>&copy; - <?php 
+    	echo date('Y');
+    	echo "  ";
+    	echo "$companyName";
+    	?>
+    </h4>
+    </div>
+    <div class="col-md-6">
+    	<h4>Hand Crafted with &heartsuit; at AS007</h4>
+	</div>
+    </div>
+   </div><!--Footer buttom closed -->
+	</div>
+</footer>
+</div>
+</body>
+</html>
