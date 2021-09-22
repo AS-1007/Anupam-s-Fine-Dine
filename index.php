cb5d505fffe66e5f8866fd4699b548ef2cada15d
Anupam Srivastava <srivastavanupam9473@gmail.com>
2021-09-22 16:36:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" type="text/css" href="assests/style.css">
+  <section id="banner">
+    <div class="main-wrapper">
+    <br><br>
+    <div class="container">
+      <div class="row">
+      <div class="col-md-6"><br><br>
+   <img src="assests/images/banner.jpg" alt="banner" class="img-responsive">
+   
+ </div>
+ <div class="col-md-6">
+  <?php 
+              include 'assests/includes/store-hours.php';
+            ?>
+           
+ </div>
+   
+ </div>
+ <h1>Welcome to Anupam's Fine Dine. Taste the best Dishes in town</h1>
+    </div>
+  </div>
+  </section><?php 
+include "assests/includes/header.php";
+?>
+
+  <br><br>
+  <hr>
+<div id="philosophy">
+  <div class="container text-center">
+    <h1>Anupam's Philosophy For Fine Dining</h1>
+    <p class="text-center ">It's just like watching a performance in a theatre: you take your seat, wait for the show to begin and enjoy the experience. During the dinner, all the ingredients are chosen and prepared by the chef himself right then and there, in front of you. It is a leap of faith into his talent, and you expect to end up with a memorable dining experience.
+
+Everyone who has visited an AS restaurant knows very well how it feels: it is a tacit agreement, a vote of confidence that is made the exact moment you cross the door. AS is predicated on the relationship between guest and chef where complete trust is placed on the chef's faculties in interpreting what is best for the guest as well as the seasonal produce and local availability.</p>
+    <p><strong>Oh ! You can call us 'The best', and your family</strong></p>
+  </div>
+
+</div>
+
+<hr>
+
+<?php 
+include "assests/includes/footer.php";
+?>
